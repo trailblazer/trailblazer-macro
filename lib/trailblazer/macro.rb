3e9e4fe043fcea443f86b2c86c1157d0b4e2ffba
Nick Sutterer <apotonick@gmail.com>
2018-03-06 13:18:58 +0100

    move all Contract-specific tests to trailblazer.

--- lib/trailblazer/macro.rb
+++ lib/trailblazer/macro.rb
@@ -1,11 +1,13 @@
-require "trailblazer"
 require "trailblazer/macro/version"
+require "trailblazer/operation"
+
+require "trailblazer/operation/inject"
+require "trailblazer/operation/input_output"
 
 require "trailblazer/operation/model"
 require "trailblazer/operation/policy"
 require "trailblazer/operation/guard"
 require "trailblazer/operation/pundit"
 require "trailblazer/operation/nested"
-require "trailblazer/operation/representer"
 require "trailblazer/operation/rescue"
 require "trailblazer/operation/wrap"
