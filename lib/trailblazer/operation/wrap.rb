15a16346913fe5e13cccc945df59ed9816031b44
Hasan Al-Banna Mohamed <kernel-io@users.noreply.github.com>
2018-03-10 20:32:10 +1300

    change Class to Module
    as a Class is a Module

--- lib/trailblazer/operation/wrap.rb
+++ lib/trailblazer/operation/wrap.rb
@@ -16,7 +16,7 @@ class Trailblazer::Operation
       include Trailblazer::Activity::Interface
 
       private def deprecate_positional_wrap_signature(user_wrap)
-        parameters = user_wrap.is_a?(Class) ? user_wrap.method(:call).parameters : user_wrap.parameters
+        parameters = user_wrap.is_a?(Module) ? user_wrap.method(:call).parameters : user_wrap.parameters
 
         return user_wrap if parameters[0] == [:req] # means ((ctx, flow_options), *, &block), "new style"
 
